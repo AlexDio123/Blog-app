54457d26cc26108dc24ddaf2538172a4594daad3
alex dionicio <alexdionicio934@hotmail.com>
2021-01-17 15:25:44 -0400

    default url added

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '######',
-        'USER': '#########',
-        'PASSWORD':'######'
+        'NAME': '#####',
+        'USER': '#####',
+        'PASSWORD':'#####'
     }
 }
 
